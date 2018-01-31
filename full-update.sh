be7131d11e6116f2fe3faeddce4be25352e0dc3f
claytoco <clayton.coery@outlook.com>
2018-01-31 11:41:08 +1100

    update config

--- full-update.sh
+++ full-update.sh
@@ -5,7 +5,7 @@ echo '                          UPDATES					   '
 echo '====================================================================='
 
 #yum updates before updating the repo
-yum makecahe fast 
+yum makecahe fast
 yum update -y
 
 echo '====================================================================='
@@ -18,10 +18,10 @@ cd /var/www/html/pub/centos/
 # echo working directory to provide feedback
 echo 'updating '$(pwd)
 # rsync the Centos 7.3.1611 directory
-rsync -avh --progress --delete --exclude={"/2*","/3*","/4*","/5*","/6*","/7.0*","/7.1*","/7.2*","/7","build*","dostools*","graphics*"} rsync://mirror.internode.on.net/centos/ .
+rsync -avh --progress --delete --exclude={"/2*","/3*","/4*","/5*","/6*","/7.0*","/7.1*","/7.2*","7.3*,"/7","build*","dostools*","graphics*"} rsync://mirror.internode.on.net/centos/ .
 #rsync -avh --progress --delete --exclude={"/2*","/3*","/4*","/5*","/6","/6.0*","/6.1*","/6.2*/","/6.3*","/6.4*","/6.5*","/6.6*","/6.7*","/7.0*","/7.1*","7.2*","/7","build*","dostools*","graphics*"} rsync://ftp.iinet.net.au/centos/ .
 # chcon
-chcon -R -t httpd_sys_content_t /var/www/html/pub/centos/7.3.1611
+chcon -R -t httpd_sys_content_t /var/www/html/pub/centos/
 
 echo '===================================================================='
 
